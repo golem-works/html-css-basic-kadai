a9cda23fd05029a73394e942ecfc360d0f59a21b
golem-works <139732089+golem-works@users.noreply.github.com>
2023-10-31 23:47:43 +0900

    Add files via upload

---
+++ Score_Chapter04.java
@@ -0,0 +1,24 @@
+
+package kadai_004;
+
+public class Score_Chapter04 {
+    public static void main(String[] args) {
+        
+        System.out.println("Aさんの点数は" + 10 + "です");
+        System.out.println("Bさんの点数は" + 20 + "です");
+        System.out.println("Cさんの点数は" + 30 + "です");
+        System.out.println("Dさんの点数は" + 40 + "です");
+        System.out.println("Eさんの点数は" + 50 + "です");
+        System.out.println("Fさんの点数は" + 60 + "です");
+        System.out.println("Gさんの点数は" + 70 + "です");
+        System.out.println("Hさんの点数は" + 80 + "です");
+        System.out.println("Iさんの点数は" + 90 + "です");
+        System.out.println("Jさんの点数は" + 100 + "です");
+        
+        int scre = 10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100;
+        int scre2 = scre / 10;
+        System.out.println(scre2);
+        
+        
+    }
+}

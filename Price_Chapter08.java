838f86ee76b408fb48c46bb3f73b6f55816f1cc4
golem-works <139732089+golem-works@users.noreply.github.com>
2023-11-05 22:28:29 +0900

    Add files via upload

--- Price_Chapter08.java
+++ Price_Chapter08.java
@@ -1,44 +1,46 @@
+package kadai_008;
 
-package kadai_08;
-
-public class  Price_Chapter08 {
+public class Price_Chapter08 {
 	public static void main(String[] args) {
 		
-		int[] userAge  = {10,20,30,40,50,60,70,80};
-		int[] serviceCost = {1000,2000,3000,4000,5000,6000,7000,8000};
-		int age = 30;
-		int order;
+		int[] userAge = {10,20,30,40,50,60,70,80};
+		int[] serviceCost = {1000,2000, 3000,4000,5000};
+		int defaultCost = 500;
+		int age = 50;
+		int cost = defaultCost;
+		
 		
 		switch(age) {
-		case 10 :
-			order = serviceCost[0];
-			break;
-		case 20 :
-			order = serviceCost[1];
-			break;
-		case 30 :
-			order = serviceCost[2];
-			break;
-		case 40 :
-			order = serviceCost[3];
-			break;
-		case 50 :
-			order = serviceCost[4];
-			break;
-		case 60 :
-			order = serviceCost[5];
-			break;
-		case 70 :
-			order = serviceCost[6];
-			break;
-		case 80 :
-			order = serviceCost[7];
-			break;
-		default:
-			order = 500;
-			break;
-			}
-		System.out.println( age + "代の料金は" + order + "円です");
 		
+		case 10:
+			cost = serviceCost[0]; 
+		break;
+		case 20:
+			cost = serviceCost[1];
+			break;
+		case 30:
+			cost = serviceCost[2];
+			break;
+		case 40:
+			cost = serviceCost[2];
+			break;
+		case 50:
+			cost = serviceCost[3];
+			break;
+		case 60: 
+			cost = serviceCost[3];
+			break;
+		case 70:
+			cost = serviceCost[3];
+			break;
+		case 80:
+			cost = serviceCost[4];
+			break;
+		default :
+			cost = defaultCost;
+			break;
+	}
+	
+	System.out.println(age + "代の料金は" + cost + "円");
 	}
 }
